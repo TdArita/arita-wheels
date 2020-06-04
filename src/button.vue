05fd14ba48071d2e4eff80cbe99ff15d43222735
TdArita <tang601619508@gmail.com>
2020-06-04 21:38:57 +0800

    验证iconPosition是左还是右还有输错的情况

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,28 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{ [`icon-${iconPosition}`]: true }">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`" />
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
 export default {
-  name: ""
-};
+  // props: ['icon', 'iconPosition'],
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(value) {
+        //属性检查器
+        return value === 'left' || value === 'right'
+      },
+    },
+  },
+}
 </script>
 <style lang="scss">
 .g-button {
@@ -14,6 +32,10 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
 
   &:hover {
     border-color: var(--border-color-hover);
@@ -23,6 +45,23 @@ export default {
   }
   &:focus {
     outline: none;
+  }
+  > .content {
+    order: 2;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.1em;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.1em;
+    }
+    > .content {
+      order: 1;
+    }
   }
 }
 </style>
